0b2e8815c22522f2bb2f27a4a6c0a1d8d150690c
Denis <ivashchenkodenis2016@gmail.com>
2026-01-12 17:31:18 +0300

    Убрал лишнее суммирование на каждой итерации

--- code/calculations/EOS/PREOS.py
+++ code/calculations/EOS/PREOS.py
@@ -364,7 +364,7 @@ class PREOS(EOS):
             else:
                 for component in self.fugacity_by_roots[root].keys():
                     gibbs_energy_by_roots.append(math.exp(self.fugacity_by_roots[root][component]) * self.zi[component])
-                    normalized_gibbs_energy[root] = sum(gibbs_energy_by_roots)
+                normalized_gibbs_energy[root] = sum(gibbs_energy_by_roots)
 
         return normalized_gibbs_energy 
 
